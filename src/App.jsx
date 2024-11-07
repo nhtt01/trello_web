ebae022cba49144ba649b49490f620c68d257947
nhtt01 <nhtantai01@gmail.com>
2024-11-07 22:15:50 +0700

    dark_light_p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
-import { Button } from '@mui/material'
-import { useColorScheme } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+import {useColorScheme} from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
 
-
-function ModeToggle() {
+function ModelSelect() {
   const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Age"
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <div style={{ display:'flex', alignItems:'center', gap:'8px' }}>
+            <LightModeIcon/> Light
+          </div>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display:'flex', alignItems:'center', gap:'8px' }}>
+            <DarkModeOutlinedIcon/> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display:'flex', alignItems:'center', gap:'8px' }}>
+            <SettingsBrightnessIcon/> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
+
 
 function App() {
   return (
     <>
-      < ModeToggle />
+      < ModelSelect />
       <div>Tấn Tài</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
