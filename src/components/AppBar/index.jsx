d2f5de39a2359ed4c7c28a41cd8fc0e825ba627a
TanTai <nguyenhoangtantai01@gmail.com>
2024-11-19 22:09:15 +0700

    Create AppBar P1

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,17 +1,28 @@
 import Box from '@mui/material/Box'
 import ModelSelect from '~/components/ModelSelect'
-
+import AppsIcon from '@mui/icons-material/Apps'
+import SvgIcon from '@mui/material/SvgIcon'
+import { ReactComponent as TrelloIcon } from '~/assets/trello-icon.svg'
+import Typography from '@mui/material/Typography'
 function AppBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: 'primary.light',
         width: '100%',
         height: (theme) => theme.trelloCustom.appBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent:'space-between'
       }}
     >
+      <Box>
+        <AppsIcon sx={{ color:'primary.main' }}/>
+        <Box sx={{ display: 'flex', alignItems: 'center'}}>
+          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color:'primary.main' }}/>
+          <Typography variant='span'>Trello</Typography>
+        </Box>
+      </Box>
       <ModelSelect />
     </Box>
   )
