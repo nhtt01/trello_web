a49087b5002d0d15c18c1fa01b59c09b2c55de10
nhtt01 <nhtantai01@gmail.com>
2024-11-25 23:23:29 +0700

    Create AppBar P2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -4,6 +4,17 @@ import AppsIcon from '@mui/icons-material/Apps'
 import SvgIcon from '@mui/material/SvgIcon'
 import { ReactComponent as TrelloIcon } from '~/assets/trello-icon.svg'
 import Typography from '@mui/material/Typography'
+import WorkSpace from './Menus/WorkSpace'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Badge from '@mui/material/Badge'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profile from './Menus/Profile'
 function AppBar() {
   return (
     <Box
@@ -16,14 +27,31 @@ function AppBar() {
         justifyContent:'space-between'
       }}
     >
-      <Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap:2 }}>
         <AppsIcon sx={{ color:'primary.main' }}/>
-        <Box sx={{ display: 'flex', alignItems: 'center'}}>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap:0.5 }}>
           <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color:'primary.main' }}/>
-          <Typography variant='span'>Trello</Typography>
+          <Typography variant='span' sx={{fontSize:'1.5rem', fontWeight:'bold', color:'primary.main'}}>Trello</Typography>
         </Box>
+        <WorkSpace/>
+        <Recent/>
+        <Starred/>
+        <Templates/>
+        <Button variant="outlined">Create</Button>
       </Box>
-      <ModelSelect />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap:2 }}>
+        <TextField id="outlined-search" label="Search..." size='small' type="search" />
+        <ModelSelect />
+        <Tooltip title="Notification">
+          <Badge color="secondary" variant="dot" sx={{ cursor:'pointer' }} >
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title="Help">
+          <HelpOutlineIcon sx={{ cursor:'pointer' }}/>
+        </Tooltip>
+        <Profile/>
+      </Box>
     </Box>
   )
 }
